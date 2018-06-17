85918c39a7272f5a8cd937b0b9e386a9af616383
wadexhong <wadexhong@gmail.com>
2018-06-17 17:01:15 +0800

    Notification 功能實作

--- app/src/main/java/com/example/wade8/myapplication/MainActivity.java
+++ app/src/main/java/com/example/wade8/myapplication/MainActivity.java
@@ -1,5 +1,14 @@
 package com.example.wade8.myapplication;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +16,8 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private final CharSequence CHANNEL_NAME = "NAME";
+    private final String CHANNEL_ID = "ID";
 
     private Button mSendNotificationButton;
     private Button mStartServiceButton;
@@ -14,10 +25,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button mBindServiceButton;
     private Button mUnBindServiceButton;
 
+    private NotificationManager mNotificationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         mSendNotificationButton = findViewById(R.id.button_send_notification);
         mStartServiceButton = findViewById(R.id.button_start_service);
@@ -37,6 +52,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()){
 
             case R.id.button_send_notification:
+
+                Intent notificationIntent = new Intent(this, MainActivity.class);
+
+                PendingIntent intent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                Notification.Builder builder = new Notification.Builder(this)
+                          // statusbar 上面顯示的那種小縮圖
+                          .setSmallIcon(R.drawable.logo_main)
+                          // 類似mipmap較大的圖
+                          .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.mipmap))
+                          .setContentTitle("測試")
+                          .setContentText("嘻嘻")
+                          // 點擊發送 PendingIntent
+                          .setContentIntent(intent)
+                          // 點擊後自動取消 Notification
+                          .setAutoCancel(true);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+                    NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+                    channel.setDescription("啥");
+                    // 所平是否可見
+                    channel.setLockscreenVisibility(Notification.VISIBILITY_SECRET);
+                    // LED燈和顏色
+                    channel.enableLights(true);
+                    channel.setLightColor(Color.RED);
+
+                    mNotificationManager.createNotificationChannel(channel);
+                    builder.setChannelId(CHANNEL_ID);
+                }
+
+
+
+                Notification notification = builder.build();
+                mNotificationManager.notify(1, notification);
+
                 break;
 
             case R.id.button_start_service:
